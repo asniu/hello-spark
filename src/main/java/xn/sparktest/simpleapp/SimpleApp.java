39cee807a31ee400d20c24c6f1be22a10932debf
asniu <asdeen123@gmail.com>
2019-06-09 22:56:39 -0300

    Added code

--- src/main/java/xn/sparktest/simpleapp/SimpleApp.java
+++ src/main/java/xn/sparktest/simpleapp/SimpleApp.java
@@ -1,8 +1,15 @@
 package xn.sparktest.simpleapp;
 
+import java.util.function.Consumer;
+
 import org.apache.spark.api.java.*;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.launcher.SparkLauncher;
+
+import xn.sparktest.simpleapp.function.StringToTwoInteger;
+import xn.sparktest.simpleapp.function.SimpleVoidFunction;
+import xn.sparktest.simpleapp.model.TwoIntegers;
 
 public class SimpleApp {
   public static void main(String[] args) {
@@ -12,14 +19,30 @@ public class SimpleApp {
     String logFile = args[0]; // Should be some file on your system
     SparkConf conf = new SparkConf();
     JavaSparkContext sc = new JavaSparkContext(conf);
+//    System.out.println("memory" + sc.getConf().get("SparkLauncher.DRIVER_MEMORY"));
     JavaRDD<String> logData = sc.textFile(logFile).cache();
 
     long numAs = logData.filter( s -> s.contains("a") ).count();
 
     long numBs = logData.filter( s -> s.contains("b") ).count();
+    
+    StringToTwoInteger strToTwoInteger = new StringToTwoInteger();
+    SimpleVoidFunction function2 = new SimpleVoidFunction();
+    logData.map(strToTwoInteger).foreach(function2);
+    
+    Consumer<? super String> action = new Consumer<Object>() {
+
+		@Override
+		public void accept(Object t) {
+			// TODO Auto-generated method stub
+			System.out.println("t: " + t.toString());
+		}
+    
+    };
+	logData.collect().forEach(action);
 
     System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
     
-    sc.stop();
+//    sc.stop();
   }
 }
